351d446af6236ed21935a4c02dfa08c578ac42af
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-16 17:41:14 +0300

    Add a user profile

--- award/views.py
+++ award/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .forms import *
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views  here.
+@login_required(login_url='/accounts/login/')
 def home(request):
 
     return render (request,'home.html')
--- award_project/urls.py
+++ award_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^award/',include('award.urls'))
+    url('^',include('award.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
