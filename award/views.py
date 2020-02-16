86f01c92d7e5c6f1862f1341680d912c40dd0d3b
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-16 19:01:29 +0300

    Add form to update the profile

--- award/views.py
+++ award/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,Http404
 from .forms import *
 from .models import Profile
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 
 # Create your views  here.
@@ -15,6 +16,22 @@ def search(request):
 
     return render(request,'search.html')
 
+@login_required
 def profile(request):
-
-    return render(request,'profile.html')    
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES),
+        if u_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm()
+        context = {
+            'u_form': u_form,
+            'p_form': p_form,
+    }
+    return render(request, 'profile.html', context) 
+  
