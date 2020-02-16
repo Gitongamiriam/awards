5d0991901e218ca210a0a3e5ffc643e28211d966
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-16 19:19:01 +0300

    Add an update for the profile

--- award/forms.py
+++ award/forms.py
@@ -1,1 +1,16 @@
-from 
+from django import forms
+from django.contrib.auth import get_user_model
+from . models import *
+
+User = get_user_model()
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["prof_pic"]        
--- award_project/settings.py
+++ award_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'award.apps.AwardConfig',
     'bootstrap3',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
