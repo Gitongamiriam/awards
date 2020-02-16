c8d1ad893e7c0afbba0cdfd9d331e1722669e808
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-16 21:30:21 +0300

    Add a field for bio

--- award/models.py
+++ award/models.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     prof_pic = models.ImageField(upload_to='images/')
+    bio = models.TextField(blank=True)
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ['bio']
+
+        
 
     def __str__(self):
        return f'{self.user.username} Profile'
