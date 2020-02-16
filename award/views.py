e2eb0d4d508ad4f7fd2be58cdd21328c21030705
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-16 13:26:00 +0300

    Add a h1 header

--- award/views.py
+++ award/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse,Http404
 # Create your views  here.
 def home(request):
 
-    return HttpResponse('welcome to moringa tribune')
+    return render (request,'home.html')
 
