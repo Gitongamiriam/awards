d723c951ef1ab9ec48fc4f0b74831cb365dd0cdc
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-20 12:12:15 +0300

    deployment

--- award_project/settings.py
+++ award_project/settings.py
@@ -16,6 +16,9 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -134,6 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
